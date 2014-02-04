90396d417f04d4aec2461cd9785553b460d394e7
Bryan Hadro <bboru21@hotmail.com>
2014-02-04 07:56:29 -0500

    implemented localStorage functionality

--- libs/js/ticker.js
+++ libs/js/ticker.js
@@ -1,11 +1,63 @@
-( function() {
+( function(s) {
+	
+	"use strict";
+	
+	// wrap the local storage functionality
+	var tickerStorage = {
+		"getStorageAsObject": function() {
+			
+			var str = s.getItem("ticker") || "{}";
+			
+			return JSON.parse( str );
+		},
+		"getDisplayName": function(name) {
+			var ticker = tickerStorage.getStorageAsObject();
+			return ticker[name].displayName;
+		},
+		"getDuration": function(name) {
+			var ticker = tickerStorage.getStorageAsObject();
+			return ticker[name].duration;
+		},
+		"setDuration": function(name, duration) {
+			var ticker = tickerStorage.getStorageAsObject();
+			ticker[name].duration = duration;
+			
+			return s.setItem( "ticker", JSON.stringify(ticker) );
+			
+		},
+		"delete": function(name) {
+			var ticker = tickerStorage.getStorageAsObject();
+			
+			delete ticker[name];
+			
+			return s.setItem( "ticker", JSON.stringify(ticker) );
+		},
+		"create": function(name, options) {
+			options = options || {};
+			
+			var ticker = tickerStorage.getStorageAsObject();
+			
+			if (!ticker[name]) {
+				ticker[name] = {};
+				ticker[name].duration = options.duration || 0;
+				ticker[name].displayName = options.displayName;
+			}
+			
+			return s.setItem( "ticker", JSON.stringify(ticker) );
+		}
+	};
+	
+	var Ticker = function(element, options) {
 		
-	var Ticker = function(element) {
-		
+		this._name = options.name;
+		this._displayName = options.displayName;
 		this._element = element;
 		this._durationContainer = this._element.getElementsByClassName("ticker-duration")[0];
-		this._duration = 0;
+		this._duration = options.duration || 0;
 		this._paused = true;
+		
+		// store
+		tickerStorage.create(this._name, {"duration": this._duration, "displayName": this._displayName});
 		
 		this.init();
 	}
@@ -17,6 +69,8 @@
 	Ticker.prototype.tick = function() {
 		if (!this._paused) {
 			this._duration++;
+			
+			//tickerStorage.setDuration(this._name, this._duration);
 			
 			this._durationContainer.innerHTML = this.toHHMMSS( this._duration );
 			this._timeout = setTimeout( this.tick.bind(this), 1000);
@@ -37,6 +91,7 @@
 	Ticker.prototype.clear = function() {
 		this.pause.call(this);
 		this._element.parentNode.removeChild(this._element);
+		tickerStorage.delete(this._name);
 	};
 	
 	Ticker.prototype.toHHMMSS = function (sec_num) {
@@ -53,7 +108,15 @@
 	
 	var instances = [];
 	
-	window.initTicker = function(tickerName, pauseExisting) {
+	window.initTicker = function(options) {
+	
+		options = options || {};
+		options.pauseExisting = options.pauseExisting || true;
+		options.duration = options.duration || null;
+		//options.paused = options.paused || false;
+		options.displayName = options.displayName || "";
+
+		options.name = options.displayName.length === 0 ? "ticker-"+new Date().getTime() : options.displayName.replace(/\s/g,"-").toLowerCase();
 		
 		var list = document.getElementById("ticker-list");
 		if (!list) {
@@ -63,7 +126,7 @@
 			document.getElementById("ticker-container").appendChild(list);
 		}
 		
-		if (pauseExisting) {
+		if (options.pauseExisting) {
 			for(var i = 0, len = instances.length; i < len; i++) {
 				instances[i].pause.call(instances[i]);
 			}
@@ -89,12 +152,14 @@
 		// duration
 		var durationContainer = document.createElement("h2");
 		durationContainer.className = "ticker-duration";
+		if (!!options.duration) { durationContainer.innerHTML = options.duration; }
 		
 		// name
-		tickerName = tickerName.length===0 ? "" : " - " + tickerName;
 		var nameContainer = document.createElement("h2");
 		nameContainer.className = "ticker-name";
-		nameContainer.appendChild( document.createTextNode(tickerName));
+		
+		if (options.displayName.length > 0) { nameContainer.innerHTML = "&nbsp;-&nbsp;"; }
+		nameContainer.appendChild( document.createTextNode(options.displayName));
 		
 		// clear button
 		var clearButton = document.createElement("button");
@@ -111,8 +176,10 @@
 		
 		list.insertBefore(item, list.firstChild);
 		
+		// need to add options here
 		// instantiate Ticker and add event listeners
-		var ticker = new Ticker(item);
+		var ticker = new Ticker(item, options);
+		
 		playButton.addEventListener("click", function() {
 			item.className = "active";
 			ticker.play.call(ticker);
@@ -128,5 +195,33 @@
 		
 		instances.push(ticker);
 	};
+	
+	// add existing tickers on dom loaded
+	document.addEventListener("DOMContentLoaded", function(e) {
+		var ticker = tickerStorage.getStorageAsObject();
+		var duration;
+		var displayName;
+		
+		for(var name in ticker) {
+			
+			duration = tickerStorage.getDuration(name);
+			displayName = tickerStorage.getDisplayName(name);
+			
+			initTicker({ "name": name, "duration": duration, "displayName": displayName });
+		}
+	});
+	
+	// store tickers on beforeunload
+	window.addEventListener("beforeunload", function(e) {
+		var name, duration;
+		
+		for(var i = 0, len = instances.length; i < len; i++) {
+			duration = instances[i]._duration;
+			name = instances[i]._name;
+			tickerStorage.setDuration(name, duration);
+		}
+		
+		return;
+	});
 
-}());
+}(window.localStorage));
